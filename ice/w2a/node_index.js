64cc85b788ad61875c8034e9ecedbaf403c27762
Amiel Devora <am409985@ucf.edu>
2020-01-15 10:06:34 -0500

    changed

--- ice/w2a/node_index.js
+++ ice/w2a/node_index.js
@@ -1,11 +1,19 @@
-function scopeDemo() {
-    console.log(x)
+console.log("Hello World!");
+var x = 1;
+var y = [1];
+function scopeDemo(y1) {
+    console.log(x);
     let a = 3, b = 4, c = 5;
     console.log(a);
     console.log(b);
     console.log(c);
+    console.log(y1);
     var x = 2;
     if(true) {
         var x = 0;
     }
+    return a;
 }
+console.log(scopeDemo(y)+10);
+console.log(y);
+console.log();
