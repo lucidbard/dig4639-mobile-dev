337481d28ccf8ec63204f21473f670980e8fe440
Zerospace <zerospacedubs@gmail.com>
2020-01-29 12:23:33 -0500

    ICE-4: TODO Co-authored-by: justmekalin <mekalinpaengwan@knights.ucf.edu>

--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -11,6 +11,17 @@ function runOnLoad()
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
+    let myTask = new Task(
+        {
+            content: "Finished ICE Exercise",
+            done: false
+        }
+    )
+    var container = document.getElementById("taskList");
+    var newtask = Document.createElement("div");
+    newtask.innerHTML = myTask.render();
+    container.appendChild(newtask);
+    console.log(myTask.render());
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
---
+++ ice/w4/src/task.js
@@ -0,0 +1,14 @@
+class task {
+        constructor(props)  {
+            this.props = props;
+        }
+
+        render(){
+            return `<span>
+            <input type ="checkbox"></input>
+            <span>${this.props.content}</span>
+            </span>`;
+        }
+}
+
+export default task;
