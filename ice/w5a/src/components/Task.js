c1869b33170083d3aab5b7a356d33b81445c76b0
Zerospace <zerospacedubs@gmail.com>
2020-02-03 12:24:44 -0500

    co authored by Mekalin Paengwan

--- ice/w5a/src/components/Task.js
+++ ice/w5a/src/components/Task.js
@@ -1,20 +1,21 @@
 class Task {
-    constructor(props) {
-        this.props = props;
-        this.element = document.createElement("div");
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.addEventListener("change",this.inputChanged)
-        this.element.appendChild(checkbox);
-        let taskText = document.createElement("span");
-        taskText.innerHTML = inputElement.value;
-       this.element.appendChild(taskText);
-    }
+    constructor() {
 
     
-    render() {
-        return this.element;
+    
+    this.props = props;
+    this.element = document.createElement("div");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change",this.inputChanged);
+    this.element.appendChild(checkbox);
+    let taskText = document.createElement("span");
+    taskText.innerHTML = inputElement.value;
+   this.element.appendChild(taskText);
+
     }
 
 }
-    export default Task;
+
+
+export default Task
