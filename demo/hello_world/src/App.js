e4828a0080e751205d8331f349e8644136173b10
Zerospace <zerospacedubs@gmail.com>
2020-02-10 12:23:24 -0500

    issue #95

---
+++ demo/hello_world/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function NameBadge(props)  {
+  console.log(props);
+  return(
+    <p>My name is {props.name}</p> 
+  )
+}
+
+
+
+class App extends React.Component {
+  clickHandler = () => {
+    alert("Clicked +");
+    console.log(this);
+  }
+  
+
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World
+        </p>
+        <NameBadge name="Stephen"/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+
+}
+}
+export default App;
