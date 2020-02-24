88c4a5fda6e44e656cecb5912aa4c6cd51eb5ae3
Zerospace <zerospacedubs@gmail.com>
2020-02-23 19:27:29 -0500

    lab 3

---
+++ labs/3/client/src/App.js
@@ -0,0 +1,10 @@
+  
+import React from 'react';
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (<Card content="This is a card!"></Card>);
+}
+
+export default App;
